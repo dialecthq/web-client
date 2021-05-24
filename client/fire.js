36b5908b70216238e49ff5258f440170ea894586
Ryan Brewer <ryanbrew13@gmail.com>
2021-05-23 22:36:26 -0400

    Added firebase configuration file (#1)

---
+++ client/fire.js
@@ -0,0 +1,18 @@
+
+import firebase from 'firebase';
+const dotenv = require('dotenv');
+dotenv.config();
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAL0vqWpH2JodSSUB2_ASKIMxm_4iysUNA",
+    authDomain: "langi-a36fc.firebaseapp.com",
+    projectId: "langi-a36fc",
+    storageBucket: "langi-a36fc.appspot.com",
+    messagingSenderId: "859597918218",
+    appId: "1:859597918218:web:c9a919be8571ca65374ff3",
+    measurementId: "G-0RW6GPSGDS"
+};
+
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire;
