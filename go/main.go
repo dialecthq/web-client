ef7fe4c5e0e9aaadaaada705d9e1351014eb09d8
Ryan Brewer <ryanbrew13@gmail.com>
2021-07-04 14:43:10 -0400

    added /user/register route

--- go/main.go
+++ go/main.go
@@ -12,10 +12,11 @@ import (
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
+	"firebase.google.com/go/auth"
 
 	"google.golang.org/api/option"
 )
-var App, Firestore = initializeApp()
+var App, Firestore, Auth = initializeApp()
 
 func main() {
 	defer Firestore.Close()
@@ -23,12 +24,13 @@ func main() {
 	// /user - all user endpoints
 	http.HandleFunc("/user/check/email", handleCheckEmail)
 	http.HandleFunc("/user/check/username", handleCheckUsername)
+	http.HandleFunc("/user/register", handleRegister)
 
     log.Println("Listing for requests at http://localhost:9000/")
 	log.Fatal(http.ListenAndServe(":9000", nil))
 }
 
-func initializeApp()(*firebase.App, *firestore.Client) {
+func initializeApp()(*firebase.App, *firestore.Client, *auth.Client) {
 	ctx := context.Background()
 	opt := option.WithCredentialsFile("./service.json")
 	app, err :=firebase.NewApp(ctx, nil, opt)
@@ -41,7 +43,9 @@ func initializeApp()(*firebase.App, *firestore.Client) {
 		panic(err)
 	}
 
-	return app, firestore
+	auth, err := app.Auth(ctx)
+
+	return app, firestore, auth
 }
 
 // Set headers for request
@@ -62,6 +66,7 @@ func ValidateReq(w *http.ResponseWriter, req *http.Request, route string)(string
 	fmt.Printf("[%v] - %v\n", timeString, route)
 	return "Successfully validated request", nil
 }
+
 
 func handleCheckEmail(w http.ResponseWriter, req *http.Request) {
 	message, err := ValidateReq(&w, req, "/user/check/email")
@@ -133,4 +138,76 @@ func handleCheckUsername(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write(jsonData)
 	}
+}
+
+func handleRegister(w http.ResponseWriter, req *http.Request) {
+	message, err := ValidateReq(&w, req, "/user/register")
+	if err != nil {
+		fmt.Printf(message)
+		return 
+	}
+
+	type language struct {
+		Key int `json:"key"`
+		Level int `json:"level"`
+	}
+
+	type newUser struct {
+		Country int `json:"country"`
+		Email string `json:"email"`
+		Name string `json:"name"`
+		Password string `json:"password"`
+		Timezone int `json:"timezone"`
+		Username string `json:"username"`
+		Languages []language `json:"languages"`
+
+	}
+
+	type resultJson struct {
+		Message string `json:"message"`
+		User newUser `json:"user"`
+	}
+
+	var user newUser
+
+	err = json.NewDecoder(req.Body).Decode(&user)
+    if err != nil {
+		jsonData, _ := json.Marshal(resultJson{Message: "Successfully created user", User: user})
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonData)
+        return
+	}
+	
+	params := (&auth.UserToCreate{}).
+        Email(user.Email).
+		Password(user.Password)
+		
+	u, err := Auth.CreateUser(context.Background(), params)
+
+	if err != nil {
+		jsonData, _ := json.Marshal(resultJson{Message: "Successfully created user"})
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonData)
+		return
+	}
+
+	_, err = Firestore.Collection("users").Doc(u.UID).Set(context.Background(), map[string]interface{}{
+		"name": user.Name,
+		"email": user.Email,
+		"timezone": user.Timezone,
+		"country": user.Country,
+		"languages": user.Languages,
+		"username": user.Username,
+	})
+
+	if err != nil {
+		jsonData, _ := json.Marshal(resultJson{Message: "Successfully created user"})
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonData)
+        return 
+	}
+
+	jsonData, _ := json.Marshal(resultJson{Message: "Successfully created user", User: user})
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonData)
 }
