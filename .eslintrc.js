f5f16b332e6bafb8455cfd6f20240afa78e932f9
Ryan Brewer <ryanbrew13@gmail.com>
2021-09-15 08:38:56 -0400

    added next eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: ['plugin:@next/next/recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true
