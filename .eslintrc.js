46feaedb77611df7b8d8a6ca74aa53a22dde14da
Ryan Brewer <ryanbrew13@gmail.com>
2021-09-16 21:53:14 -0400

    added livekit deploy server keys to .env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
-require("@babel/register");
+require("@babel/register")
 
 function isTruthy(value) {
-  if (!value) return false;
-  return ["1", "true"].indexOf(value.toLowerCase()) >= 0;
+  if (!value) return false
+  return ["1", "true"].indexOf(value.toLowerCase()) >= 0
 }
 
-const OFF = "off";
-const ERROR = "error";
-const WARNING = isTruthy(process.env.CI) ? ERROR : "warn";
+const OFF = "off"
+const ERROR = "error"
+const WARNING = isTruthy(process.env.CI) ? ERROR : "warn"
 
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: ["plugin:@next/next/recommended"],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: "module"
   },
   plugins: ["react"],
   rules: {
@@ -28,7 +28,6 @@ module.exports = {
     eqeqeq: [WARNING, "allow-null"],
     "import/imports-first": OFF,
     // 'import/no-unresolved': OFF,
-    quotes: ["error", "single"],
     indent: [WARNING, 2, { SwitchCase: 1 }],
     "max-len": [WARNING, 100, 2],
     "no-console": [WARNING, { allow: ["warn", "error"] }],
@@ -39,15 +38,14 @@ module.exports = {
     "no-var": ERROR,
     "prefer-const": WARNING,
     "react/prop-types": [WARNING, { ignore: ["className"] }],
-    "import/no-unresolved": WARNING,
     semi: [WARNING, "never"],
-    "react/no-unescaped-entities": [OFF],
+    "react/no-unescaped-entities": [OFF]
   },
   settings: {
     "import/resolver": {
       node: {
-        paths: ["src"],
-      },
-    },
-  },
-};
+        paths: ["src"]
+      }
+    }
+  }
+}
--- src/utils/state/userContainer.js
+++ src/utils/state/userContainer.js
@@ -1,57 +1,54 @@
 /* eslint-disable no-shadow */
 /* eslint-disable max-len */
-import { useState, useEffect } from "react";
-import { createContainer } from "unstated-next";
-import firebase from "firebase";
-import {
-  uniqueNamesGenerator,
-  adjectives,
-  colors,
-  animals,
-} from "unique-names-generator";
-import fire from "../fire";
-import strings from "../data/strings";
-import rooms from "../data/rooms";
-import axios from "axios";
+import { useState, useEffect } from "react"
+import { createContainer } from "unstated-next"
+import firebase from "firebase"
+import { uniqueNamesGenerator, adjectives, colors, animals } from "unique-names-generator"
+import fire from "../fire"
+import strings from "../data/strings"
+import rooms from "../data/rooms"
+import axios from "axios"
 
 function useUser() {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [premium, setPremium] = useState(false);
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [premium, setPremium] = useState(false)
 
   const authStateChanged = async (authState) => {
     if (!authState) {
-      setUser(null);
-      setLoading(false);
-      return;
+      setUser(null)
+      setLoading(false)
+      return
     }
 
-    setLoading(true);
-    const userRef = await fire
-      .firestore()
-      .collection("users")
-      .doc(authState.uid)
-      .get();
-    await axios.get(
-      `/api/check_subscription?customer_id=${userRef.data().stripe_id}`
-    );
-    setUser(userRef.data());
-    setLoading(false);
-  };
+    setLoading(true)
+    const userRef = await fire.firestore().collection("users").doc(authState.uid).get()
+    if (userRef.data().subscription_id) {
+      const resultRef = await axios.get(
+        `/api/check_subscription?subscription_id=${userRef.data().subscription_id}`
+      )
+      if (resultRef.data.active) {
+        setPremium(true)
+      }
+    }
+
+    setUser(userRef.data())
+    setLoading(false)
+  }
 
   // listen for Firebase state change
   useEffect(() => {
-    const unsubscribe = fire.auth().onAuthStateChanged(authStateChanged);
-    return () => unsubscribe();
-  }, []);
+    const unsubscribe = fire.auth().onAuthStateChanged(authStateChanged)
+    return () => unsubscribe()
+  }, [])
 
   return {
     user,
     setUser,
-    loading,
-  };
+    loading
+  }
 }
 
-const User = createContainer(useUser);
+const User = createContainer(useUser)
 
-export default User;
+export default User
