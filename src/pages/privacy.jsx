c38675c16f2a7d1d317a548b85dca948f0edafd2
Ryan Brewer <ryanbrew13@gmail.com>
2021-09-17 11:32:42 -0400

    added metas

--- src/pages/privacy.jsx
+++ src/pages/privacy.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import Footer from '../components/landing/common/Footer'
-import Header from '../components/landing/common/Header'
-import Content from '../components/landing/privacy/Content'
-import CallToAction from '../components/landing/common/CallToAction'
+import React from "react"
+import Footer from "../components/landing/common/Footer"
+import Header from "../components/landing/common/Header"
+import Content from "../components/landing/privacy/Content"
+import CallToAction from "../components/landing/common/CallToAction"
+import Seo from "../components/seo/Seo"
 
 const Privacy = () => (
   <>
+    <Seo
+      title="Privacy Policy - Learn a new language today"
+      description="Give the gift of language."
+    />
     <Header />
     <Content />
     <CallToAction />
--- src/pages/terms.jsx
+++ src/pages/terms.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import Footer from '../components/landing/common/Footer'
-import Header from '../components/landing/common/Header'
-import Content from '../components/landing/tos/Content'
-import CallToAction from '../components/landing/common/CallToAction'
+import React from "react"
+import Footer from "../components/landing/common/Footer"
+import Header from "../components/landing/common/Header"
+import Content from "../components/landing/tos/Content"
+import CallToAction from "../components/landing/common/CallToAction"
+import Seo from "../components/seo/Seo"
 
 const TOS = () => (
   <>
+    <Seo title="Profile - Learn a new language today" description="Give the gift of language." />
     <Header />
     <Content />
     <CallToAction />
