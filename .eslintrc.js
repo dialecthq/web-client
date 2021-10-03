be8046fb56962d53e16c3c981eb0f49f05138030
Ryan Brewer <ryanbrew13@gmail.com>
2021-10-03 17:40:32 -0400

    hotfix: changed about page text

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,5 @@
 require("@babel/register")
 
-function isTruthy(value) {
-  if (!value) return false
-  return ["1", "true"].indexOf(value.toLowerCase()) >= 0
-}
-
-const OFF = "off"
-const ERROR = "error"
-const WARNING = isTruthy(process.env.CI) ? ERROR : "warn"
-
 module.exports = {
   env: {
     browser: true,
@@ -23,29 +14,4 @@ module.exports = {
     sourceType: "module"
   },
   plugins: ["react"],
-  rules: {
-    "comma-dangle": OFF,
-    eqeqeq: [WARNING, "allow-null"],
-    "import/imports-first": OFF,
-    // 'import/no-unresolved': OFF,
-    indent: [WARNING, 2, { SwitchCase: 1 }],
-    "max-len": [WARNING, 100, 2],
-    "no-console": [WARNING, { allow: ["warn", "error"] }],
-    "no-debugger": WARNING,
-    "no-fallthrough": WARNING,
-    "no-unreachable": WARNING,
-    "no-unused-vars": [WARNING, { vars: "all", args: "none" }],
-    "no-var": ERROR,
-    "prefer-const": WARNING,
-    "react/prop-types": [WARNING, { ignore: ["className"] }],
-    semi: [WARNING, "never"],
-    "react/no-unescaped-entities": [OFF]
-  },
-  settings: {
-    "import/resolver": {
-      node: {
-        paths: ["src"]
-      }
-    }
-  }
 }
